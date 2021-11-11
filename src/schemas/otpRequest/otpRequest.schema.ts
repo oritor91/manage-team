39eb86b2a947e95cf84a9004fb051b5fd3fec8bd
Ori Tor <oritor91@gmail.com>
2021-11-11 12:29:33 +0200

    adding OtpRequest schema

---
+++ src/schemas/otpRequest/otpRequest.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type OtpRequestDocument = OtpRequest & Document;
+
+@Schema()
+export class OtpRequest {
+  @Prop({required: true})
+  phone: string;
+
+  @Prop({required: true})
+  code: Number;
+
+  @Prop({required: true})
+  expiryTime: Date;
+
+}
+
+export const OtpRequestSchema = SchemaFactory.createForClass(OtpRequest);
